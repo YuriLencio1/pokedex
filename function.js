8e6f9e3700c15801f7d413cbff6f4c8ad416732c
Yuri Eduardo Lencio <yurilencio@ciandt.com>
2025-10-20 13:37:25 -0300

    Adicionando botões next&prev

--- function.js
+++ function.js
@@ -2,28 +2,63 @@ const pokemonName = document.querySelector(".pokemon_name");
 const pokemonId = document.querySelector(".pokemon_id");
 const pokemonGif = document.querySelector(".pokemon_gif");
 const pokemonform = document.querySelector(".form");
-const pokemonInput = document.querySelector(".nameornumber"); 
+const pokemonInput = document.querySelector(".nameornumber");
+
+const pokemonNext = document.querySelector(".btn-next");
+const pokemonPrev = document.querySelector(".btn-prev");
 
 const fetchPokemon = async (pokemon) => {
   const APIpokemon = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   );
-  const dado = await APIpokemon.json();
-  return dado;
+
+  if (APIpokemon.status == 200) {
+    const dado = await APIpokemon.json();
+    return dado;
+  }
 };
 
 const renderPokemon = async (pokemon) => {
+
+  pokemonName.innerHTML = "loading...";
+  pokemonId.innerHTML = "";
+  pokemonGif.innerHTML = ""
+
   const data = await fetchPokemon(pokemon);
-  pokemonName.innerHTML = data.name;
-  pokemonId.innerHTML = data.id;
-  pokemonGif.src =
-    data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
-      "front_default"
-    ];
+  if (data) {
+    pokemonGif.style.display = "block"
+    pokemonName.innerHTML = data.name;
+    pokemonId.innerHTML = data.id;
+    pokemonGif.src =
+      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+        "front_default"
+      ];
+  } else {
+    pokemonGif.style.display = "none"
+    pokemonName.innerHTML = "Não encontrado";
+    pokemonId.innerHTML = "";
+  }
 };
 
-pokemonform.addEventListener('submit', (event) => {
+
+
+pokemonform.addEventListener("submit", (event) => {
   event.preventDefault();
 
-   renderPokemon(pokemonInput.value)
+  renderPokemon(pokemonInput.value.toLowerCase());
+  pokemonInput.value = "";
 });
+
+pokemonNext.addEventListener("click", () =>
+  alert('next clicked')
+
+);
+
+pokemonPrev.addEventListener("click", () =>
+  alert('prev clicked')
+
+);
+  
+
+renderPokemon('1')
+
